baa665a3cf0e19f6e3bac7c6ed7c7dbc0accef1b
Raymond Thompson <60783171+Thealltommo@users.noreply.github.com>
2022-08-29 11:42:31 +0100

    Update loveCalculator.js

--- loveCalculator.js
+++ loveCalculator.js
@@ -2,5 +2,13 @@ let firstPerson = prompt("What is the first person's name?");
 let secondPerson = prompt("What is the seond person's name?");
 
 let love = Math.random();
-love = love * 100;
-alert("You are a " + (Math.floor(love) + "% match"));
+love = love * 100 + 1;
+
+
+if (love > 95){
+  alert("You are a " + (Math.floor(love) + "% match and are perfect together!"));
+}if(love < 94 && love > 50){
+  alert("You are a " + (Math.floor(love) + "% match and should be very happy"));
+}else{
+  alert("You are a " + (Math.floor(love) + "% match and may not last"));
+}
