6f36f60bfd84c1a439a6aafdcb3b22a2a26f13a1
Raymond Thompson <60783171+Thealltommo@users.noreply.github.com>
2022-08-29 11:24:36 +0100

    Create loveCalculator.js

---
+++ loveCalculator.js
@@ -0,0 +1,6 @@
+let firstPerson = prompt("What is the first person's name?");
+let secondPerson = prompt("What is the seond person's name?");
+
+let love = Math.random();
+love = love * 100;
+alert("You are a " + (Math.floor(love) + "% match"));
